1ff56f2444a22afabac24384fb4b02577fc4a8a8
Yousef Roshandel <yousef.r1999@gmail.com>
2020-05-05 19:22:44 +0430

    draw a white triangle

--- ep01.py
+++ ep01.py
@@ -1,4 +1,6 @@
 import glfw
+from OpenGL.GL import *
+import numpy as np
 
 # Iinitializing glfw library
 if not glfw.init():
@@ -15,6 +17,27 @@ if not window:
 # Set window's position
 glfw.set_window_pos(window, 400, 200)
 
+# Make the context current
+glfw.make_context_current(window)
+
+#! My Codes
+
+vertices = [-0.5, -0.5, 0.0,
+            0.5, -0.5, 0.0,
+            0.0, 0.5, 0.0]
+
+colors = [1.0, 0.0, 0.0,
+          0.0, -0.5, 0.0,
+          0.0, 0.5, 0.0]
+
+vertices = np.array(vertices, dtype=np.float32)
+colors = np.array(colors, dtype=np.float32)
+
+glEnableClientState(GL_VERTEX_ARRAY) 
+glVertexPointer(3, GL_FLOAT, 0, vertices)
+
+glClearColor(0, 0.1, 0.1, 1)
+
 # Loop until the user closes the window
 while not glfw.window_should_close(window):
     # Render here, e.g. using pyOpenGL
@@ -24,6 +47,11 @@ while not glfw.window_should_close(window):
 
     # Poll for and process events
     glfw.poll_events()
+
+    #! My events
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glDrawArrays(GL_TRIANGLES, 0, 3)
 
 # Termiante glfw
 glfw.terminate()
