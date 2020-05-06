76d8611b50b744519d38fddc704a077ae65f5bf1
Yousef Roshandel <yousef.r1999@gmail.com>
2020-05-06 19:59:20 +0430

    triangle with programmable pipeline

--- triangle.py
+++ triangle.py
@@ -1,60 +1,80 @@
 import glfw
 from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
 
-# Iinitializing glfw library
+vertex_src = """
+# version 330
+in vec3 a_position;
+in vec3 a_color;
+out vec3 v_color;
+void main()
+{
+    gl_Position = vec4(a_position, 1.0);
+    v_color = a_color;
+}
+"""
+
+fragment_src = """
+# version 330
+in vec3 v_color;
+out vec4 out_color;
+void main()
+{
+    out_color = vec4(v_color, 1.0);
+}
+"""
+
+# initializing glfw library
 if not glfw.init():
-    raise Exception("glfx can not be initialized!")
+    raise Exception("glfw can not be initialized!")
 
-# Create the window
-window = glfw.create_window(1280, 780, "My OpenGL window", None, None)
+# creating the window
+window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
 
-# Check if window was created
+# check if window was created
 if not window:
     glfw.terminate()
-    raise Exception("glfw window can not be created")
+    raise Exception("glfw window can not be created!")
 
-# Set window's position
+# set window's position
 glfw.set_window_pos(window, 400, 200)
 
-# Make the context current
+# make the context current
 glfw.make_context_current(window)
 
-#! My Codes
-
-vertices = [-0.5, -0.5, 0.0,
-            0.5, -0.5, 0.0,
-            0.0, 0.5, 0.0]
-
-colors = [1.0, 0.0, 0.0,
-          0.0, 1.0, 0.0,
-          0.0, 0.0, 1.0]
+vertices = [-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
+             0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+             0.0,  0.5, 0.0, 0.0, 0.0, 1.0]
 
 vertices = np.array(vertices, dtype=np.float32)
-colors = np.array(colors, dtype=np.float32)
 
-glEnableClientState(GL_VERTEX_ARRAY) 
-glVertexPointer(3, GL_FLOAT, 0, vertices)
+shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
 
-glEnableClientState(GL_COLOR_ARRAY)
-glColorPointer(3, GL_FLOAT, 0, colors)
+VBO = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, VBO)
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
 
+position = glGetAttribLocation(shader, "a_position")
+glEnableVertexAttribArray(position)
+glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+
+color = glGetAttribLocation(shader, "a_color")
+glEnableVertexAttribArray(color)
+glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
+glUseProgram(shader)
 glClearColor(0, 0.1, 0.1, 1)
 
-# Loop until the user closes the window
+# the main application loop
 while not glfw.window_should_close(window):
-    # Render here, e.g. using pyOpenGL
-
-    # Swap front and back buffers
-    glfw.swap_buffers(window)
-
-    # Poll for and process events
     glfw.poll_events()
 
-    #! My events
     glClear(GL_COLOR_BUFFER_BIT)
 
     glDrawArrays(GL_TRIANGLES, 0, 3)
 
-# Termiante glfw
+    glfw.swap_buffers(window)
+
+# terminate glfw, free up allocated resources
 glfw.terminate()
